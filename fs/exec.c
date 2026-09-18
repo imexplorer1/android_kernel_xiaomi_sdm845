523ac3dfe99e182e8dd77b0bdcdc390ca2776742
imexplorer1 <imexplorer@126.com>
2026-09-18 13:17:04 +0800

    add resukisu mamual hook

--- fs/exec.c
+++ fs/exec.c
@@ -1673,6 +1673,15 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+#ifdef CONFIG_KSU_MANUAL_HOOK
+__attribute__((hot))
+extern int ksu_handle_execve(int *fd, const char *filename,
+				void *argv, void *envp, int *flags);
+__attribute__((hot))
+extern int ksu_handle_post_execve(int *fd, const char *filename,
+				void *argv, void *envp, int *flags, int *retval);
+#endif
+
 /*
  * sys_execve() executes a new program.
  */
@@ -1686,6 +1695,10 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
+	
+#ifdef CONFIG_KSU_MANUAL_HOOK
+	ksu_handle_execve((int *)AT_FDCWD, filename, &argv, &envp, 0);
+#endif
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
@@ -1833,6 +1846,9 @@ out_files:
 	if (displaced)
 		reset_files_struct(displaced);
 out_ret:
+#ifdef CONFIG_KSU_MANUAL_HOOK
+	ksu_handle_post_execve((int *)AT_FDCWD, filename, &argv, &envp, 0, &retval);
+#endif
 	putname(filename);
 	return retval;
 }
