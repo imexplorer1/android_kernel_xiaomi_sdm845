a2a7734e157dcacf7c3b5bffcc5fc90c9c2bdb8d
imexplorer1 <imexplorer@126.com>
2026-09-18 17:44:42 +0800

    11111111

--- fs/exec.c
+++ fs/exec.c
@@ -1853,13 +1853,30 @@ out_ret:
 	return retval;
 }
 
+#ifdef CONFIG_KSU_MANUAL_HOOK
+__attribute__((hot))
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr,
+			       void *argv, void *envp, int *flags);
+__attribute__((hot))
+extern int ksu_handle_post_execveat(int *fd, struct filename **filename_ptr,
+				    void *argv, void *envp, int *flags, int *retval);
+#endif
+
 int do_execve(struct filename *filename,
 	const char __user *const __user *__argv,
 	const char __user *const __user *__envp)
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
+#ifdef CONFIG_KSU_MANUAL_HOOK
+	int retval;
+	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+	retval = do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
+	ksu_handle_post_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0, &retval);
+	return retval;
+#else
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
+#endif
 }
 
 int do_execveat(int fd, struct filename *filename,
@@ -1886,7 +1903,15 @@ static int compat_do_execve(struct filename *filename,
 		.is_compat = true,
 		.ptr.compat = __envp,
 	};
+#ifdef CONFIG_KSU_MANUAL_HOOK // 32-bit ksud and 32-on-64 support
+	int retval;
+	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+	retval = do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
+	ksu_handle_post_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0, &retval);
+	return retval;
+#else
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
+#endif
 }
 
 static int compat_do_execveat(int fd, struct filename *filename,
